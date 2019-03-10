583fb15830ac845c561158877695c6191afaa52f
cvachha <cyrus.vachha@yahoo.com>
2019-03-09 23:06:42 -0800

    Unity Project added
    Added the project which includes work in progress idea 8

---
+++ Kinshapes VR Game V1 Github/Assets/lightBeamScript.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class lightBeamScript : MonoBehaviour
+{
+    public LineRenderer laserLineRenderer;
+    public float laserWidth = 0.1f;
+    public float laserMaxLength = 5f;
+
+    public Vector3 origin;
+    public Vector3 direction;
+
+    private RaycastHit rayInfo;
+    private lightBeamScript reflection;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+       // origin = gameObject.GetComponent<Transform>().position;
+       // direction= new Vector3(-90,0,0);
+        rayInfo = new RaycastHit();
+
+        //laser
+        Vector3[] initLaserPositions = new Vector3[2] { origin, Vector3.zero };
+        laserLineRenderer.SetPositions(initLaserPositions);
+        laserLineRenderer.SetWidth(laserWidth, laserWidth);
+        laserLineRenderer.enabled = true;
+        ///
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(origin, direction);
+        if (Physics.Raycast(ray, out rayInfo))
+        {
+            if (rayInfo.transform.gameObject.tag == "mirror") // use whatever logic here to test for hitting a mirror
+            {
+                Debug.Log("Hit mirror!");
+                if (reflection == null)
+                {
+                    reflection = new lightBeamScript();
+                }
+                reflection.origin = rayInfo.point;
+                reflection.direction = Vector3.Reflect(direction, rayInfo.normal);
+
+                //laser
+                Vector3[] nextLaserPositions = new Vector3[2] { rayInfo.point, Vector3.zero };
+                laserLineRenderer.SetPositions(nextLaserPositions);
+                //
+            }
+            Debug.DrawLine(origin, rayInfo.point, Color.red);
+        }
+        else
+        {
+            Destroy(reflection);
+            Debug.DrawRay(origin, direction, Color.blue);
+        }
+    }
+}
+*/
+
+public class lightBeamScript : MonoBehaviour
+{
+    public Vector3 origin;
+    public Vector3 direction;
+
+    private RaycastHit rayInfo;
+    private lightBeamScript reflection;
+
+    private void Start()
+    {
+
+        origin = gameObject.GetComponent<Transform>().position;
+        // direction= new Vector3(-90,0,0);
+        rayInfo = new RaycastHit();
+    }
+    void Update()
+    {
+        Ray ray = new Ray(origin, direction);
+        
+        if (Physics.Raycast(ray, out rayInfo))
+
+        {
+            if (rayInfo.transform.tag == "mirror") // use whatever logic here to test for hitting a mirror
+            {
+                if (reflection == null)
+                {
+                    reflection = new lightBeamScript();
+                }
+
+                Vector3 incomingVec = rayInfo.point - origin;
+               // reflection.direction = Vector3.Reflect(rayInfo.point, rayInfo.transform.forward);
+               // reflection.direction = Vector3.Reflect(direction, rayInfo.transform.forward);
+                reflection.direction = Vector3.Reflect(direction, rayInfo.normal);
+
+                Debug.Log("hitting mirror");
+
+                reflection.origin = rayInfo.point;
+               // reflection.direction = Vector3.Reflect(direction, rayInfo.normal);
+                Debug.DrawLine(rayInfo.point, reflection.direction, Color.red);
+
+            }
+            Debug.Log("outside hit mirror");
+            Debug.DrawLine(rayInfo.point, reflection.direction, Color.blue);
+
+            //Debug.DrawLine(origin, rayInfo.point, Color.blue);
+        }
+        else
+        {
+            Destroy(reflection);
+            Debug.DrawRay(origin, direction, Color.white);
+        }
+    }
+}
---
+++ Kinshapes VR Game V1 Github/Assets/mirrorColliderScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirrorColliderScript : MonoBehaviour
+{
+
+    public bool stay = true;
+    public bool exit = true;
+    public bool canMoveMirror = false;
+    private float stayCount = 0.0f;
+    public GameObject mirrorFrame;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        mirrorFrame.name = "mirrorStand";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canMoveMirror)
+        {
+            if (Input.GetKey(KeyCode.J))
+            {
+                print("left arrow key is held down");
+                mirrorFrame.transform.Rotate(0, 0.3f, 0, Space.Self);
+            }
+
+            if (Input.GetKey(KeyCode.L))
+            {
+                print("right arrow key is held down");
+                mirrorFrame.transform.Rotate(0,-0.3f, 0,Space.Self);
+
+            }
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (stay)
+        {
+            if (stayCount > 0.25f)
+            {
+                canMoveMirror = true;
+                Debug.Log("staying");
+                stayCount = stayCount - 0.25f;
+
+
+            }
+            else
+            {
+                stayCount = stayCount + Time.deltaTime;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (exit)
+        {
+            canMoveMirror = false;
+            Debug.Log("exit");
+        }
+    }
+
+}
